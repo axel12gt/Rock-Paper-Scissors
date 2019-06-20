53820d66544e67a635ba53bbfadfffb8a949d799
Dameon Brown <axel12gt@live.com>
2019-06-20 00:16:39 -0400

    added some pretty sweet buttons

--- assets/js/code.js
+++ assets/js/code.js
@@ -18,24 +18,24 @@
 
     // game object
     var game = {
-        // renders page
+        // renders start page
         start: function () {
             var header = $("<div>")
             header.addClass("text-center headerT bg-primary")
             header.text("Rock Paper Scissor World Championship")
             $("body").append(header)
-
+            // calls bootstrap container class and puts it in dom
             var container = $("<div>")
             container.addClass("container mt-5")
             header.after(container)
-
+            // appends table div to container
             var table = $("<div>")
             container.append(table)
-
+            // sets a row for the table
             var row1 = $("<div>")
             row1.addClass("row text-center")
             table.append(row1)
-
+            // sets a column to the row
             var column1 = $("<div>")
             column1.addClass("col-6")
             row1.append(column1)
@@ -43,9 +43,9 @@
             var column2 = $("<div>")
             column2.addClass("col-6")
             row1.append(column2)
-
-            var img1 = $('<img>')
-            img1.attr('src', 'assets/imgs/pcimg.png')
+            // sets the image to the column
+            var img1 = $("<img>")
+            img1.attr("src", "assets/imgs/pcimg.png")
             img1.attr("height", "100px")
             img1.attr("width", "100px")
             img1.addClass("vs")
@@ -57,13 +57,13 @@
             img2.attr("width", "100px")
             img2.addClass("vs")
             column2.append(img2)
-
-            var vsComputer = $('<h1>')
-            vsComputer.text('Vs Computer')
+            //sets the buttons to the img
+            var vsComputer = $("<h1>")
+            vsComputer.append("<button type='button'>Computer</button>")
             column1.append(vsComputer)
 
-            var online = $('<h1>')
-            online.text('Online')
+            var online = $("<h1>")
+            online.append("<button type='button'>Online</button>")
             column2.append(online)
         }
 
